d03a3c16dbb32e93364a1ca5df4c2a522e93dde0
Robert Bojor <robert.bojor@gmail.com>
2019-08-23 15:22:00 +0300

    Added logic for subtracting a slice from another

---
+++ difference.go
@@ -0,0 +1,64 @@
+package intersect
+
+// SubtractString - subtract a string slice from another
+func SubtractString(fromSlice, subtractSlice []string) (result []string) {
+	if len(fromSlice) == 0 {
+		return
+	}
+	if len(subtractSlice) == 0 {
+		result = fromSlice
+		return
+	}
+	hash := make(map[string]bool)
+	for _, e := range fromSlice {
+		hash[e] = true
+	}
+	for _, e := range subtractSlice {
+		if !hash[e] {
+			result = append(result, e)
+		}
+	}
+	return
+}
+
+// SubtractInt - subtract an int slice from another
+func SubtractInt(fromSlice, subtractSlice []int) (result []int) {
+	if len(fromSlice) == 0 {
+		return
+	}
+	if len(subtractSlice) == 0 {
+		result = fromSlice
+		return
+	}
+	hash := make(map[int]bool)
+	for _, e := range fromSlice {
+		hash[e] = true
+	}
+	for _, e := range subtractSlice {
+		if !hash[e] {
+			result = append(result, e)
+		}
+	}
+	return
+}
+// SubtractInt64 - subtract an int64 slice from another
+func SubtractInt64(fromSlice, subtractSlice []int64) (result []int64) {
+	if len(fromSlice) == 0 {
+		return
+	}
+	if len(subtractSlice) == 0 {
+		result = fromSlice
+		return
+	}
+	hash := make(map[int64]bool)
+	for _, e := range fromSlice {
+		hash[e] = true
+	}
+	for _, e := range subtractSlice {
+		if !hash[e] {
+			result = append(result, e)
+		}
+	}
+	return
+}
+
---
+++ helpers.go
@@ -0,0 +1,57 @@
+package intersect
+
+func uniqueStrings(input []string) []string {
+	u := make([]string, 0, len(input))
+	m := make(map[string]bool)
+	for _, val := range input {
+		if _, ok := m[val]; !ok {
+			m[val] = true
+			u = append(u, val)
+		}
+	}
+	return u
+}
+func uniqueInts(input []int) []int {
+	u := make([]int, 0, len(input))
+	m := make(map[int]bool)
+	for _, val := range input {
+		if _, ok := m[val]; !ok {
+			m[val] = true
+			u = append(u, val)
+		}
+	}
+	return u
+}
+func uniqueInt64s(input []int64) []int64 {
+	u := make([]int64, 0, len(input))
+	m := make(map[int64]bool)
+	for _, val := range input {
+		if _, ok := m[val]; !ok {
+			m[val] = true
+			u = append(u, val)
+		}
+	}
+	return u
+}
+func uniqueFloat32s(input []float32) []float32 {
+	u := make([]float32, 0, len(input))
+	m := make(map[float32]bool)
+	for _, val := range input {
+		if _, ok := m[val]; !ok {
+			m[val] = true
+			u = append(u, val)
+		}
+	}
+	return u
+}
+func uniqueFloat64s(input []float64) []float64 {
+	u := make([]float64, 0, len(input))
+	m := make(map[float64]bool)
+	for _, val := range input {
+		if _, ok := m[val]; !ok {
+			m[val] = true
+			u = append(u, val)
+		}
+	}
+	return u
+}
